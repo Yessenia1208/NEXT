7248ead6cab5fa26cbc71ba8dac3a8acb0a21bf2
Yessenia1208 <yessi2017jesus@gmail.com>
2023-07-12 14:14:28 -0600

    Prueba de cambios

---
+++ src/app/registro/page.js
@@ -0,0 +1,43 @@
+'use client'
+import { useState } from "react";
+import "./registro.css"
+
+export default function registro() {
+
+    const [state, setState] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setState({
+            ...state,
+            [name]: value
+        })
+    }
+
+    return (
+        <div className="container">
+
+            <input 
+                onChange={handleChange} 
+                name="email" 
+                value={state.email} 
+                type="text" 
+                placeholder="email" 
+            />
+
+            <input 
+                name="password" 
+                onChange={handleChange} 
+                type="password" 
+                placeholder="123456" 
+            />
+
+            <button>Login</button>
+
+        </div>
+    )
+
+}
