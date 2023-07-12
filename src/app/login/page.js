572fc129ff59b32f2ef1bee1e03a940e5294e3d2
Yessenia1208 <yessi2017jesus@gmail.com>
2023-07-12 13:28:53 -0600

    Avance 12 de julio

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,17 +1,43 @@
+'use client'
+import { useState } from "react";
 import "./login.css"
 
 export default function Login() {
 
+    const [state, setState] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setState({
+            ...state,
+            [name]: value
+        })
+    }
+
     return (
-      <div className="container">
-        <h1>Iniciar sesión</h1>
-        <label  for="">Correo </label>
-        <input placeholder="1234ejemplo@gmail.com" type="text"></input>
-        <label  for="">Password </label>
-        <input placeholder="1234ejemplo@" type="password"></input>
-        <button  type="Submit">Iniciar sesión</button>
-      </div>
+        <div className="container">
+
+            <input 
+                onChange={handleChange} 
+                name="email" 
+                value={state.email} 
+                type="text" 
+                placeholder="email" 
+            />
+
+            <input 
+                name="password" 
+                onChange={handleChange} 
+                type="password" 
+                placeholder="123456" 
+            />
+
+            <button>Login</button>
+
+        </div>
     )
 
-  
-    }
+}
--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -16,5 +16,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+export default app;
+
+
 
 
